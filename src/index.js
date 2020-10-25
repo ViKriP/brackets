7cc4a0267726cddae08712c33bb86578f0bddb08
Viktor <viktor_biz@ukr.net>
2020-10-25 23:56:22 +0200

    feat: implement function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var newStr = '';
+  var bracketsPair = [];
+  var result = false;
+  var openingBracketCount;
+
+  if (str.lenght % 2 !== 0) {
+    return result;
+  }
+
+  openingBracketCount = str.lenght / 2;
+
+
+  for (var i = 0; i < openingBracketCount; i++){
+    bracketsPair = getBracketsPair(bracketsConfig, str[i]);
+    if (str[i] !== bracketsPair[0] && str[str.length - i + 1 ] !== bracketsPair[1]) {
+      return result;
+    }
+  }
+  
+  return true;
+}
+
+function getBracketsPair(bracketsConfig, openingBracket) {
+  for (var i = 0; i < bracketsConfig.lenght; i++){
+    if (bracketsConfig[i][0] === openingBracket) {
+      return bracketsConfig[i];
+    }
+  }
 }
